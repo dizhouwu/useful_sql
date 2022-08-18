1fe43fc32fd1b4ad0a0eacb528b52eceab16153c
BoLack Litterman <samwu0808@outlook.com>
2022-08-18 15:55:58 -0500

    Create rating_change.sql

---
+++ rating_change.sql
@@ -0,0 +1,16 @@
+create temporary table fuck ( rating VARCHAR,  d date);
+
+
+insert into fuck (rating, d) values ('A', '2022-07-01');
+insert into fuck (rating, d) values ('A', '2022-08-01');
+insert into fuck (rating, d) values ('B', '2022-08-05');
+insert into fuck (rating, d) values ('C', '2022-08-09');
+
+select fuck.d as startTime,
+       lead(fuck.d) over (order by fuck.d) as endTime,
+       fuck.rating
+from (select fuck.*, 
+             lag(rating) over (order by d) as prev_rating
+      from fuck
+     ) fuck
+where prev_rating is null or prev_rating <> rating;
